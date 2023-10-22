54d0c8fdb42307d6fd7a3fda44e943158bd402ff
Arthur Vinicius Carneiro Nunes <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-10-22 04:28:11 +0000

    Lendo o HTML de maneira mais sofisticada

--- Cartas/cartas.go
+++ Cartas/cartas.go
@@ -1,9 +1,10 @@
 package main
 
-
 import (
 	"fmt"
+	"io"
 	"math/rand"
+	"os"
 	"strconv"
 	"sync"
 
@@ -28,26 +29,25 @@ func main() {
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		c.Type("html", "utf-8") // a codificação UTF-8 é para consertar problemas de texto
-		htmlContent := `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<meta charset="UTF-8"> 
-			<title>Gerar Senha</title>
-		</head>
-		<body>
-			<form action="/gerar_senha" method="post">
-				<input type="submit" value="Obter Senha">
-			</form>			
-			<p>Sua senha é: <span id="senha">%s</span></p>	
-			<a href="/nova_pagina">Ir para a Segunda Página</a>		
-		</body>
-		</html>
-		`
-		//senhaCorreta é adicionada ao %s e html enviadr como resosta
-		return c.SendString(fmt.Sprintf(htmlContent, senha_correta))
+		// Abrindo o arquivo HTML da pasta
+		file, err := os.Open("static/inicio.html")
+		if err != nil {
+			// Lidar com erros, como arquivo não encontrado
+			return c.SendString("Erro ao carregar o arquivo HTML")
+		}
+		defer file.Close()
 
+		// Lendo o conteúdo do arquivo
+		htmlContent, err := io.ReadAll(file)
+		if err != nil {
+			// Lidar com erros de leitura
+			return c.SendString("Erro ao ler o arquivo HTML")
+		}
+
+		//senhaCorreta é adicionada ao %s e html enviado como resposta
+		return c.SendString(fmt.Sprintf(string(htmlContent), senha_correta))
 	})
+
 	//rota para a ação de gerar nova senha
 	app.Post("/gerar_senha", func(c *fiber.Ctx) error {
 		go GerarSenha()
@@ -58,26 +58,24 @@ func main() {
 	})
 	//rota para confirmar senha
 	app.Post("/confirmar_senha", func(c *fiber.Ctx) error {
-		//obtem senha digitada pelo usuário e faz a comparação com a correta
+		// obtem senha digitada pelo usuário e faz a comparação com a correta
 		senha_digitada := c.FormValue("senha_usuario")
 		//retorna ou para a pagina onde mostra a carta ou para uma mensagem de erro dependendo da comparação
 		c.Type("html", "utf-8")
-		htmlContent := `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<meta charset="UTF-8">
-			<title>Nova Página</title>
-		</head>
-		<body>
-			<form action="/confirmar_senha" method="post">
-			<input type="text" name="senha_usuario" placeholder="Digite sua senha">
-			<input type="submit" value="Confirmar">
-			</form>
-			<h2>Sua carta é: %s</h2>
-		</body>
-		</html>
-		`
+		// Abrindo o arquivo HTML da pasta
+		file, err := os.Open("static/confirma-senha.html")
+		if err != nil {
+			// Lidar com erros, como arquivo não encontrado
+			return c.SendString("Erro ao carregar o arquivo HTML")
+		}
+		defer file.Close()
+
+		// Lendo o conteúdo do arquivo
+		htmlContent, err := io.ReadAll(file)
+		if err != nil {
+			// Lidar com erros de leitura
+			return c.SendString("Erro ao ler o arquivo HTML")
+		}
 		// conversão para inteiro, utlizamos '_' para ignorar o segundo valor de retorno ( variável do tipo error que indica se a conversão foi bem-sucedida, podemos implementar futuramenete)
 		index, _ := strconv.Atoi(senha_correta)
 		// indices começam em 0, por isso a subtração
@@ -87,7 +85,7 @@ func main() {
 		// carta recebe a carta sorteada
 		carta := cartas[index]
 		if senha_digitada == senha_correta {
-			return c.SendString(fmt.Sprintf(htmlContent, carta))
+			return c.SendString(fmt.Sprintf(string(htmlContent), carta))
 		} else {
 			return c.SendString("Senha incorreta. Tente novamente!")
 		}
@@ -95,22 +93,21 @@ func main() {
 	//definição rota segunda_pagina
 	app.Get("/nova_pagina", func(c *fiber.Ctx) error {
 		c.Type("html", "utf-8")
-		htmlContent := `
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<meta charset="UTF-8">
-			<title>Nova Página</title>
-		</head>
-		<body>
-			<form action="/confirmar_senha" method="post">
-			<input type="text" name="senha_usuario" placeholder="Digite sua senha">
-			<input type="submit" value="Confirmar">
-			</form>
-		</body>
-		</html>
-		`
-		return c.SendString(fmt.Sprintf(htmlContent))
+		// Abrindo o arquivo HTML da pasta
+		file, err := os.Open("static/page-2.html")
+		if err != nil {
+			// Lidar com erros, como arquivo não encontrado
+			return c.SendString("Erro ao carregar o arquivo HTML")
+		}
+		defer file.Close()
+
+		// Lendo o conteúdo do arquivo
+		htmlContent, err := io.ReadAll(file)
+		if err != nil {
+			// Lidar com erros de leitura
+			return c.SendString("Erro ao ler o arquivo HTML")
+		}
+		return c.SendString(fmt.Sprintf(string(htmlContent)))
 	})
 
 	app.Listen(":3000")
